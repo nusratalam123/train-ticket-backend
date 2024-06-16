c61b1e9c3c780e36573a1e39293982369e07ce8e
nusratalam <nusratalam975@gmail.com>
2024-06-16 22:12:38 +0600

    update user model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,16 +1,17 @@
-import bcrypt from "bcrypt";
-import CryptoJS from "crypto-js";
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
     user_id: {
       type: String,
+    },
+    email: {
+      type: String,
       required: true,
-      trim: true,
-      minLength: [3, "name must be at least 3 characters"],
-      maxLength: [100, "name is too large"],
+      unique: true,
+      validate: [validator.isEmail, "provide a valid email"],
     },
     user_Name: {
       type: String,
@@ -21,14 +22,36 @@ const userSchema = new mongoose.Schema(
     },
     balance: {
       type: String,
+    },
+    password: {
+      type: String,
       required: true,
-      trim: true,
+      minLength: [6, "password must be at least 6 characters"],
+    },
+    status: {
+      type: String,
+      default: true,
     },
   },
   {
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+  } catch (err) {
+    next(err as undefined);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
